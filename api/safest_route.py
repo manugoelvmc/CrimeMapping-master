77d3f5d5f24714ea572a2ae651f40c973e39d8be
Manu Goel <manugoelvmc@gmail.com>
2019-03-03 09:24:10 +0530

    fix

--- api/safest_route.py
+++ api/safest_route.py
@@ -48,11 +48,11 @@ def calculate_score_for_route(steps):
         total_score += report_safesty_score
     return total_score
 
-def getRouteSafety(waypoints,dist):
+def getRouteSafety(waypoints,dist, size, data, plot_crime_points):
     rating = 0.0
     
     for i in range(len(waypoints)):
-        rating+=getPointSafety(waypoints[i])      
+        rating+=getPointSafety(waypoints[i], size, data, plot_crime_points)      
     
     return rating/len(waypoints)
 
@@ -86,7 +86,7 @@ def isnearby_police(lat,lng):
     return True
 
 
-def findStartIndex(lng):
+def findStartIndex(lng,size, data):
     low = 0
     high =size-1
     mid=0
@@ -104,13 +104,13 @@ def findStartIndex(lng):
     return mid
 
 
-def getPointSafety(point):
+def getPointSafety(point, size, data, plot_crime_points):
     radius=0.05
     rating = 0.0
     lat = point[0]
     lng = point[1]    
     
-    iloc = findStartIndex(lng-radius)
+    iloc = findStartIndex(lng-radius, size, data)
     #print(iloc)
     
     crime_points = []
@@ -203,93 +203,9 @@ def get_route(origin, destination):
             point.append(directions['routes'][i]['legs'][0]['steps'][j]['start_location']['lng'])
             waypoints.append(point)
         Rdistance = directions['routes'][i]['legs'][0]['distance']['value']
-        rating = getRouteSafety(waypoints,Rdistance)
+        rating = getRouteSafety(waypoints,Rdistance, size, data, plot_crime_points)
         if(rating<bestrate):
             bestrate=rating
             choice=i
     return routes[choice];
-    
-    # routes.append(waypoints)
-    # murder = 5
-    # rape = 4
-    # eve_tease = 2
-    # theft = 3
-    # kidnapping = 4
-    # accidents = 3
-    # camera = 1
 
-    # dataset = pd.read_csv(r"file3.csv")
-    # dataset['rate'] = murder * dataset['Murder'] + rape * dataset['Rape'] + eve_tease * dataset['Eve tease'] + theft * dataset['Theft'] + kidnapping * dataset['Kidnapping'] + accidents * dataset['Accidents'] - dataset['Camera']
-
-    # # Google MapsDdirections API endpoint
-    # endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-    # api_key = 'AIzaSyD08BN7gmi_FNMNszEWYoe6y4hzHs570VY'
-
-    # # origin1 = input('Where are you?: ')
-    # # destination1 = input('Where do you want to go?: ')
-    # origin = origin.replace(' ','+')
-    # destination = destination.replace(' ','+')
-
-    # # Building the URL for the request
-    # nav_request = 'origin={}&destination={}&key={}&alternatives={}'.format(origin,destination,api_key,'true')
-    # request = endpoint + nav_request
-
-    # # Sends the request and reads the response.
-    # response = urllib.request.urlopen(request).read()
-    
-    # # Loads response as JSON
-    # directions = json.loads(response)
-
-    # nroutes = len(directions['routes'])
-
-    # routes = []
-    # dataroutes = []
-    
-    # for i in range(nroutes):
-    #     nsteps = len(directions['routes'][i]['legs'][0]['steps'])
-    #     waypoints = []
-    #     waydata = []
-
-    #     for j in range(nsteps):   
-    #         point = []
-    #         datapoint = []
-    #         point.append(directions['routes'][i]['legs'][0]['steps'][j]['start_location']['lat'])
-    #         point.append(directions['routes'][i]['legs'][0]['steps'][j]['start_location']['lng'])
-    #         datapoint.append([directions['routes'][i]['legs'][0]['steps'][j]['start_location']['lat'],directions['routes'][i]['legs'][0]['steps'][j]['start_location']['lng']])
-    #         waypoints.append(point)
-    #         waydata.append(datapoint)
-    #     routes.append(waypoints)
-    #     dataroutes.append(waydata)
-        
-     
-    # X = dataset[['Latitude','Longitude']]
-    # Y = dataset['rate']
-
-    # clf = RadiusNeighborsRegressor(radius=0.05, weights='distance', p=2)
-    # clf.fit(X,Y)
-
-    
-    # bestyet = sys.maxsize
-    # choice = 0
-
-    # for i in range(nroutes):
-    #     sum = 0
-    #     for j in range(len(routes[i])):
-    #         sum = sum+clf.predict(dataroutes[i][j])
-    #     newrate = sum/len(routes[i])
-    #     if newrate <= bestyet:
-    #         bestyet = newrate
-    # choice = i
-
-    # return directions['routes'][choice]
-
-# def get_route(origin, destination):
-#     payload = {'origin': origin, 'destination': destination, 'key':'AIzaSyD62lqHOOFvXUfXW9Itrd-f30pSzi_Db3Q', 'alternatives': 'true'}
-#     request = requests.get('https://maps.googleapis.com/maps/api/directions/json', params=payload)
-#     route_response = request.json()
-#     routes = route_response['routes']
-#     for route in routes:
-#         steps_in_route = route["legs"][0]["steps"]
-#         score = calculate_score_for_route(steps_in_route)
-#         route["score"] = score
-#     return route_response
